328db8dd5c3b496d8fe47373ab5f8ebfcfe196db
Jamie MacDonald <jamie.macdonald@nairnacademy.net>
2024-11-19 21:09:24 +0000

    Updated Styles for about page.

--- kidfit/src/app/about/page.tsx
+++ kidfit/src/app/about/page.tsx
@@ -2,34 +2,82 @@ import Header from "../components/header";
 import AboutCard from "../components/aboutCard";
 
 export default function about() {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      <section className="flex flex-col px-10 gap-7">
-        <AboutCard
-          title="Our Mission"
-          desc="Our mission at KidFit is to provide children and adults with the
+	return (
+		<div className='flex flex-col h-screen bg-gradient-to-br from-slate-200 to-slate-300'>
+			<Header />
+			<section className='flex flex-col px-10 gap-7 justify-center h-full'>
+				<AboutCard
+					title='Our Mission'
+					icon={
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth={1.5}
+							stroke='currentColor'
+							className='size-6'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z'
+							/>
+						</svg>
+					}
+					desc='Our mission at KidFit is to provide children and adults with the
             knowlege of heathy eating, with being able to track food intake and
-            calories."
-        />
-        <div className="flex flex-row justify-end">
-          <AboutCard
-            title="What We Do"
-            desc="We specialize in designing educational and interactive mobile
+            calories.'
+				/>
+				<div className='flex flex-row justify-end'>
+					<AboutCard
+						title='What We Do'
+						icon={
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								fill='none'
+								viewBox='0 0 24 24'
+								strokeWidth={1.5}
+								stroke='currentColor'
+								className='size-6'
+							>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002'
+								/>
+							</svg>
+						}
+						desc='We specialize in designing educational and interactive mobile
               applications that promote positive habits and raise awareness
               about the importance of nutrition, exercise, and mental
-              well-being."
-          />
-        </div>
+              well-being.'
+					/>
+				</div>
 
-        <AboutCard
-          title="Why It Matters"
-          desc="With childhood obesity and related health concerns on the rise, our
+				<AboutCard
+					title='Why It Matters'
+					icon={
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth={1.5}
+							stroke='currentColor'
+							className='size-6'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z'
+							/>
+						</svg>
+					}
+					desc='With childhood obesity and related health concerns on the rise, our
             work is more important than ever. By encouraging healthier
             lifestyles through technology, we hope to foster a generation that
-            values wellness and grows into thriving adults."
-        />
-      </section>
-    </div>
-  );
+            values wellness and grows into thriving adults.'
+				/>
+			</section>
+		</div>
+	);
 }
--- kidfit/src/app/components/aboutCard.tsx
+++ kidfit/src/app/components/aboutCard.tsx
@@ -1,13 +1,16 @@
 interface paramProps {
-    title: string,
-    desc: string,
+	title: string;
+	icon: JSX.Element
+	desc: string;
 }
-
-export default function aboutCard({title, desc}: paramProps) {
-    return (
-        <div className="flex flex-col bg-zinc-50 max-w-fit p-8 rounded-2xl border-zinc-400 border-2">
-            <span className="font-poppins font-bold text-3xl">{title}</span>
-            <span className="max-w-screen-sm">{desc}</span>
-        </div>
-    )
+export default function aboutCard({ title, icon, desc }: paramProps) {
+	return (
+		<div className='flex flex-col bg-zinc-50 max-w-fit p-8 rounded-2xl border-zinc-400 border-2 select-none bg-opacity-60'>
+			<div className='flex flex-row items-center gap-2'>
+				<span className='font-poppins font-bold text-3xl'>{title}</span>
+                {icon}
+			</div>
+			<span className='max-w-screen-sm'>{desc}</span>
+		</div>
+	);
 }
